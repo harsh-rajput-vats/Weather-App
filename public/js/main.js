e23d328426cd4194354a9f46fa69b6b5ca2e370f
Harsh Rajput <harsh.rajput.vats@gmail.com>
2022-08-13 12:57:15 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,59 +1,60 @@
-const cityName = document.getElementById('cityName');
-const submitBtn = document.getElementById('submitBtn');
+const cityName=document.getElementById('cityName');
+const submitBtn=document.getElementById('submitBtn');
 
-const city_name = document.getElementById('city_name');
-const temp_real_val = document.getElementById('temp_real_val');
-const temp_status = document.getElementById('temp_status');
-const datahide = document.querySelector('.middle_layer');
+const city_name=document.getElementById('city_name');
+const temp_real_val=document.getElementById('temp_real_val');
+const temp_status=document.getElementById('temp_status');
+const datahide  = document.querySelector('.middle_layer');
 
-const getInfo = async (event) => {
+const getInfo=  async(event)=>{
     event.preventDefault();
-    let cityVal = cityName.value;
+    let cityVal=cityName.value;
 
-
-    if (cityVal === "") {
-        city_name.innerText = `Please write the City name before search`;
+    
+    if(cityVal===""){
+        city_name.innerText=`Please write the City name before search`;
         datahide.classList.add('data_hide');
 
-    } else {
-        try {
-            let url = `http://api.weatherapi.com/v1/current.json?key=ffc4bfd3d032422ab0462351221308&q=${cityVal}&aqi=no`;
-            const response = await fetch(url);
-            const data = await response.json();
-            const arrData = [data];
+    }else{
+        try{
+            
+            let url=`http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=47a6b9088d31536c121629a2006cfd33`;
+            const response=await fetch(url);
+            const data= await response.json(); 
+            const arrData=[data];
+            
+            city_name.innerText=`${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real_val.innerText= arrData[0].main.temp;
 
-            city_name.innerText = `${arrData[0].location.region}, ${arrData[0].location.country}`;
-            temp_real_val.innerText = arrData[0].current.temp_c;
-
-            const tempMood = arrData[0].current.condition.text;
+            const tempMood = arrData[0].weather[0].main;
             //condition to check sunny or cloudy
             if (tempMood == "Clear") {
                 temp_status.innerHTML =
                     "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
-            } else if (tempMood == "Clouds") {
+                } else if (tempMood == "Clouds") {
                 temp_status.innerHTML =
                     "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
-            } else if (tempMood == "Rain") {
+                } else if (tempMood == "Rain") {
                 temp_status.innerHTML =
                     "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
-            } else {
+                } else {
                 temp_status.innerHTML =
                     "<i class='fas  fa-sun' style='color:#eccc68;'></i>";
-            }
-
-            datahide.classList.remove('data_hide');
+                }
+                 
+                datahide.classList.remove('data_hide');
 
 
         }
-        catch {
-            city_name.innerText = `Please write the City name properly`;
+        catch{
+            city_name.innerText=`Please write the City name properly`;
             datahide.classList.add('data_hide');
         }
     }
 
 }
 
-submitBtn.addEventListener('click', getInfo);
+submitBtn.addEventListener('click',getInfo);
 
 
 
